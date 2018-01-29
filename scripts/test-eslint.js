c67c163f8990c40ec4400ebc343c7b74256eb8a6
scriptPilot <mail@scriptPilot.de>
2018-01-30 00:44:55 +0100

    clean-up

--- scripts/test-eslint.js
+++ scripts/test-eslint.js
@@ -21,11 +21,17 @@ if (!com.isInstalled()) folders.push('scripts');
 
 // Run test
 com.run(['node_modules/.bin/eslint', ...folders, '--config', configFile, '--output-file', 'eslint.log', '--fix', '--max-warnings', 0], (err) => {
+  // If errors
   if (err) {
+    // Log warning
     com.log.warning('ESLint found some errors. Please check file "eslint.log" for details.');
+  // If no errors
   } else {
+    // Remove eslint.log file
     if (!com.remove('eslint.log')) com.log.warning('Failed to remove the eslint.log file');
+    // Log success
     com.log.success('ESLint check passed without errors.');
   }
+  // Remove temp folder
   if (!com.remove('.temp/test-eslint')) com.log.error('Failed to remove the temp folder');
 });
